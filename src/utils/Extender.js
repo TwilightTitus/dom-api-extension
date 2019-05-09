3dc72e9f478400b68801622e5bcb53eb25d247d4
Frank Schüler <SchuelerFrank@gmx.de>
2019-05-09 20:30:11 +0200

    update

--- src/utils/Extender.js
+++ src/utils/Extender.js
@@ -3,14 +3,10 @@ const Extender = function(aName, aExtention){
 		if(!aPrototype.__extentions__)
 			aPrototype.__extentions__ = {};
 		
-		console.log("current extentions:", aPrototype.__extentions__);
-		
 		if(!aPrototype.__extentions__[aName]){
 			aPrototype.__extentions__[aName] = true;
 			aExtention(aPrototype);
 		}
-		else
-			console.log("extentions installed!");
 	}
 };
 
